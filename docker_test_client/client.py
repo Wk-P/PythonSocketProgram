03ca6bc2ea86090561228b68f3dce0b5f901c172
Wk-P <weekend2000@gmail.com>
2023-11-07 20:24:49 +0900

    update client.py

--- docker_test_client/client.py
+++ docker_test_client/client.py
@@ -2,39 +2,49 @@ import requests
 import multiprocessing
 import random
 import json
+import time
 
-def send_request(server_url, prime_sum, response_counts, lock):
+
+def send_request(server_url, prime_sum, response_counts, lock, response_time_list):
     try:
+        start_time = time.time()
         data = {
-            'num': prime_sum
+            'number': prime_sum
         }
         response = requests.post(server_url, json=data)
         if response.status_code == 200:
             # print("Request was successful. Server response:")
             json_data = response.json()
-            # print(json_data, end='\n-----\n')
-            port = json_data['server']
+            print(json_data, end='\n-----\n')
+            server = json_data['server']
 
             # update response_counts with lock
             with lock:
-                if port not in response_counts:
-                    response_counts[port] = 1
+                if server not in response_counts:
+                    response_counts[server] = 1
                 else:
-                    response_counts[port] += 1
+                    response_counts[server] += 1
+            
         else:
             print(f"Request failed with status code: {response.status_code}")
-    
+
+        end_time = time.time()
+        response_time_list.append((end_time - start_time, server))
     except Exception as e:
         print(f"Request failed with error: {str(e)}")
 
-if __name__ == "__main__":
 
+
+
+if __name__ == "__main__":
+    all_start_time = time.time()
     server_url = "http://192.168.56.102:8080"
     
     # make response_counts dictionary with Manager
     manager = multiprocessing.Manager()
     response_counts = manager.dict()
-    
+    response_time_list = manager.list()
+
     # create lock
     lock = manager.Lock()
 
@@ -43,23 +53,31 @@ if __name__ == "__main__":
     pool = multiprocessing.Pool(processes=num_processes)
     
     # requests
-    num_requests = 3000
-    request_counts = [random.randint(1, 50) for _ in range(num_requests)]
+    num_requests = 1
+    request_counts = [random.randint(1, 10000) for _ in range(num_requests)]
     
     # response result list
     results = []
-    
+
     for count in request_counts:
         if len(results) >= num_processes:
             results.pop(0).get()  # wait the first response pf requests when request queue is full (max = 4)
-        result = pool.apply_async(send_request, (server_url, count, response_counts, lock))
+        result = pool.apply_async(send_request, (server_url, count, response_counts, lock, response_time_list))
         results.append(result)
     
     # wait all prcesses finishing
     pool.close()
     pool.join()
     
+
+    file_path = "training.txt"
+    with open(file_path, 'w', encoding='utf-8') as f:
+        for i in range(num_requests):
+            f.write(f"{request_counts[i]} {response_time_list[i][0]} {response_time_list[i][1]} \n")
+
+    all_end_time = time.time()
     # print counter
     print("Response counts:")
     for server, data in response_counts.items():
-        print(f"Server {server} responses => {data}")
+            print(f"Server {server} responses => {data}")
+    print(f"Run time : {all_end_time - all_start_time: .3f} seconds")
